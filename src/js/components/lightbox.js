06b180e49eba185a3630bd229841ff84dbd4fa47
Roman Kruk <wilmlass@gmail.com>
2021-08-24 03:46:43 +0300

    corrections

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -2,6 +2,8 @@ import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
 export default function createModal(url) {
-  const instance = basicLightbox.create(`<img class="lightbox__img" src="${url}" alt="" />`);
+  const instance = basicLightbox.create(
+    `<div><img class="lightbox__img" src="${url}" alt="" /></div>`,
+  );
   instance.show();
 }
